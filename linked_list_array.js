66201f57f23060ff8bdc569b82655a126b611396
Rahmat H <kickyou44@gmail.com>
2017-07-26 17:58:44 +0700

    linked list selesai

--- linked_list_array.js
+++ linked_list_array.js
@@ -1,6 +1,32 @@
 //write your code here
+class Node {
+	constructor(value, next = null) {
+		this.value = value
+		this.next = next
+	}
+}
 
-let list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-console.log(list.getLength());
-console.log(list.getHead());
-console.log(list.getTail());
+class LinkedList {
+	constructor(length) {
+		this.list = this.generateLinkedList(length)
+	}
+
+	generateLinkedList(length) {
+		let result;
+		let temp;
+		for (let i = 1; i <= length; i++) {
+			if (result == null) {
+				result = new Node(i)
+				temp = result;
+			} else {
+				let nextNode = new Node(i)
+				temp.next = nextNode
+				temp = nextNode
+			}
+		}
+		return result
+	}
+}
+
+let list = new LinkedList(10);
+console.log(list.list);
